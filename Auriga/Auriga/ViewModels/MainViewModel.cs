f83a9f851e6f58c4865358552a29886b96944118
HMate <mhidvegi@gmail.com>
2019-08-24 21:43:39 +0200

    learn view navigation

---
+++ Auriga/Auriga/ViewModels/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Auriga.Common;
+using Auriga.Views;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Auriga.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private ICentralView _currentCentralView;
+        public ICentralView CurrentCentralView
+        {
+            get => _currentCentralView;
+            set
+            {
+                if (_currentCentralView != null)
+                {
+                    _currentCentralView = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(CurrentCentralView)));
+                }
+                else
+                {
+                    _currentCentralView = value;
+                }
+            }
+        }
+
+        public MainViewModel()
+        {
+            CurrentCentralView = new DefaultView();
+        }
+
+        public ICommand ShowGraphEditor
+        {
+            get
+            {
+                return new RelayCommand(p => CurrentCentralView = new GraphEditor());
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Auriga/Auriga/Views/ICentralView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auriga.ViewModels
+{
+    /// <summary>
+    /// Interface for views that can be positioned on the center of the application
+    /// </summary>
+    public interface ICentralView
+    {
+    }
+}
