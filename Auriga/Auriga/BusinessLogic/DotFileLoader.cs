359e4734e58c35058b9f7340ddc9f03bada21a26
Hidvegi Mate <mate.hidvegi@evosoft.com>
2019-11-08 13:41:04 +0100

    create dot loader class

---
+++ Auriga/Auriga/BusinessLogic/DotFileLoader.cs
@@ -0,0 +1,19 @@
+﻿using Auriga.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auriga.BusinessLogic
+{
+    public class DotFileLoader
+    {
+        public static Graph Load(string filePath)
+        {
+            Graph result = new Graph();
+
+
+
+            return result;
+        }
+    }
+}
