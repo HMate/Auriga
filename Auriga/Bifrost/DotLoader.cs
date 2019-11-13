843edcf3b40262b30a12fee126f24e44686933b6
HMate <mhidvegi@gmail.com>
2019-11-13 18:23:04 +0100

    try parsing

--- Auriga/Bifrost/DotLoader.cs
+++ Auriga/Bifrost/DotLoader.cs
@@ -5,16 +5,131 @@ namespace Bifrost
 {
     public class DotLoader
     {
+        private string dotString;
+        private int pos = 0;
+        private int trypos = 0;
+        private const StringComparison casei = StringComparison.OrdinalIgnoreCase;
+
+        public DotLoader(string dotString)
+        {
+            this.dotString = dotString;
+        }
+
         public static Dot.DotGraph Load(string dotString)
         {
-            Dot.DotGraph gr = new Dot.DotGraph();
+            DotLoader loader = new DotLoader(dotString);
+            return loader.Load();
+        }
 
+        public Dot.DotGraph Load()
+        {
+            Dot.DotGraph gr = new Dot.DotGraph();
             dotString = dotString.Trim();
-            string[] tokens = dotString.Split(new [] { " ", "\t", "\n", "\r", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            if(dotString.Length < 7)
+            {
+                return gr;
+            }
+
+            string[] tokens = dotString.Split(new[] { " ", "\t", "\n", "\r", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+
+            string token = readUntil(' ', '{');
+            if(token.Equals("graph", casei) || token.Equals("digraph", casei))
+            {
+                if(current() != '{')
+                {
+                    string name = readUntil('{').Trim();
+                }
+
+                while (current() != '}')
+                {
+                    tryReadNodeStatement();
+                } 
+            }
 
             return gr;
         }
 
+        private bool tryReadNodeStatement()
+        {
+            resetTryPos();
+            return tryReadID() && tryReadAttrList();
+        }
+
+        private bool tryReadAttrList()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool tryReadID()
+        {
+            while (char.IsWhiteSpace(dotString, trypos))
+                trypos++;
+            if (trypos >= dotString.Length)
+            {
+                return false;
+            }
+            if(dotString[trypos] == '"' )
+            {
+                trypos++;
+                while (!(dotString[trypos] == '"' && dotString[trypos-1] == '\\') && trypos < dotString.Length)
+                    trypos++;
+                if (trypos >= dotString.Length)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool tryReadEdgeStatement()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool tryReadAttrStatement()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool tryReadIDAssignment()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool tryReadSubGraphStatement()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void resetTryPos()
+        {
+            trypos = pos;
+        }
+
+        private void advancePos()
+        {
+            pos = trypos;
+        }
+
+        private string readUntil(params char[] separators)
+        {
+            int index = dotString.IndexOfAny(separators, pos);
+            int charCount = index - pos;
+            if (index < 0 || dotString.Length - pos - 1 < charCount)
+            {
+                charCount = dotString.Length - pos - 1;
+            }
+            string token = dotString.Substring(pos, charCount);
+            pos = index;
+            return token;
+        }
+
+        private char current()
+        {
+            return dotString[pos];
+        }
 
         public static GraphData LoadF(string dotString)
         {
